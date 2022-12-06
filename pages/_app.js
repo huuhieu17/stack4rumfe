a26b034ce22fe630e44004a8f4464a41305c3be4
Nguyen Huu Hieu <huuhieu1711@gmail.com>
2022-12-07 01:08:06 +0700

    add tailwindcss vechaiui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import { extendTheme, VechaiProvider } from "@vechaiui/react";
+import "../styles/globals.css";
+import { bee, cool, dawn, light, midnight, pale } from "themes/index";
+import Layout from "components/Layout/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const theme = extendTheme({
+    cursor: "pointer",
+    colorSchemes: {
+      bee,
+      cool,
+      dawn,
+      light,
+      midnight,
+      pale,
+    },
+  });
+  return (
+    <VechaiProvider density="compact" theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </VechaiProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
